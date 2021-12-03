ea2c0d24b64d12d5b0bdc174f275698712e28c63
Иван Александрович Стахов <50211739+LeftTry@users.noreply.github.com>
2021-12-03 22:45:23 +0300

    Some changes

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ void mergesort(vector<int>& v, int ind1, int ind2){
 }
 
 int partition(vector<int>& v, int l, int r){
-    int m = l;
+    int m = (l + r) / 2;
     while (true){
         while(v[l] < v[m]) l++;
         while(v[r] > v[m]) r--;
@@ -54,9 +54,37 @@ int partition(vector<int>& v, int l, int r){
     }
 }
 
+int partition1(vector<int>& v, int l, int r){
+    int left = l;
+    int mid = (l + r) / 2;
+    int right = r;
+    int mediana;
+    if(left > mid){
+        if(left < right){
+            mediana = left;
+        }else mediana = right;
+    }else if(mid > right){
+        mediana = right;
+    }else mediana = mid;
+    while (true){
+        while(v[l] < v[mediana]) l++;
+        while(v[r] > v[mediana]) r--;
+        if(l >= r) return r;
+        swap(v[l++], v[r--]);
+    }
+}
+
 void quickSort(vector<int>& v, int l, int r){
     if(l < r){
         int m = partition(v, l, r);
+        quickSort(v, l, m);
+        quickSort(v, m + 1, r);
+    }
+}
+
+void quickSortMed(vector<int>& v, int l, int r){
+    if(l < r){
+        int m = partition1(v, l, r);
         quickSort(v, l, m);
         quickSort(v, m + 1, r);
     }
@@ -135,42 +163,278 @@ void test (void (*sort_function)(vector<int>&), int n, vector<int>& v){
     cout << timeUsed << endl;
 }
 
+void test (void (*sort_function)(vector<int>&, int, int), int n, vector<int>& v, int l, int r){
+    double ticksStart = clock();
+    sort_function(v, l, r);
+    double timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    sort_function(v, l, r);
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v.begin(), v.end());
+    ticksStart = clock();
+    sort_function(v, l, r);
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    std::random_device rd;
+    std::mt19937 g(rd());
+    shuffle(v.begin(), v.end() - v.size() + v.size() / 10, g);
+    ticksStart = clock();
+    sort_function(v, l, r);
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
+}
+
 int main() {
     int n = 1e3;
     cout << "Size: " << n << endl;
-    vector<int> v(n);
+    vector<int> v(n), vbackup(n);
     for(int i = 0;i < v.size();i++) v[i] = rand();
+    vbackup = v;
     cout << "BubbleSort:" << endl;
     test(bubbleSort, n, v);
+    v = vbackup;
     cout << "BubbleSortExt:" << endl;
     test(bubbleSortExt, n, v);
+    v = vbackup;
     cout << "selectSort:" << endl;
     test(selectSort, n, v);
+    v = vbackup;
     cout << "putSort:" << endl;
     test(putSort, n, v);
     n = 1e4;
     cout << "Size: " << n << endl;
     vector<int> v1(n);
     for(int i = 0;i < v1.size();i++) v1[i] = rand();
+    vbackup = v1;
     cout << "BubbleSort:" << endl;
     test(bubbleSort, n, v1);
+    v1 = vbackup;
     cout << "BubbleSortExt:" << endl;
     test(bubbleSortExt, n, v1);
+    v1 = vbackup;
     cout << "selectSort:" << endl;
     test(selectSort, n, v1);
+    v1 = vbackup;
     cout << "putSort:" << endl;
     test(putSort, n, v1);
     n = 1e5;
     cout << "Size: " << n << endl;
     vector<int> v2(n);
     for(int i = 0;i < v2.size();i++) v2[i] = rand();
+    vbackup = v2;
     cout << "BubbleSort:" << endl;
     test(bubbleSort, n, v2);
+    v2 = vbackup;
     cout << "BubbleSortExt:" << endl;
     test(bubbleSortExt, n, v2);
+    v2 = vbackup;
     cout << "selectSort:" << endl;
     test(selectSort, n, v2);
+    v2 = vbackup;
     cout << "putSort:" << endl;
     test(putSort, n, v2);
+    n = 1e5;
+    cout << "Size: " << n << endl;
+    for(int i = 0;i < v.size();i++) v[i] = rand();
+    vbackup = v;
+    cout << "PyramidSort:" << endl;
+    test(pyramidSort, n, v);
+    v = vbackup;
+    cout << "MergeSort:" << endl;
+    int o = v.size() / 2;
+    if(v.size() % 2 == 1) o++;
+    test(mergesort, n, v, 0, o);
+    v = vbackup;
+    cout << "QuickSort:" << endl;
+    test(quickSort, n, v, 0, v.size() - 1);
+    v = vbackup;
+    cout << "QuickSortMed:" << endl;
+    test(quickSortMed, n, v, 0, v.size() - 1);
+    v = vbackup;
+    cout << "QuickSortNonStable:" << endl;
+    double ticksStart = clock();
+    sort(v.begin(), v.end());
+    double timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v.begin(), v.end());
+    ticksStart = clock();
+    sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    std::random_device rd;
+    std::mt19937 g(rd());
+    shuffle(v.begin(), v.end() - v.size() + v.size() / 10, g);
+    ticksStart = clock();
+    sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
+    v = vbackup;
+    cout << "QuickSortStable:" << endl;
+    ticksStart = clock();
+    stable_sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    stable_sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v.begin(), v.end());
+    ticksStart = clock();
+    stable_sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    shuffle(v.begin(), v.end() - v.size() + v.size() / 10, g);
+    ticksStart = clock();
+    stable_sort(v.begin(), v.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
+    n = 1e6;
+    cout << "Size: " << n << endl;
+    for(int i = 0;i < v1.size();i++) v1[i] = rand();
+    vbackup = v1;
+    cout << "PyramidSort:" << endl;
+    test(pyramidSort, n, v1);
+    v1 = vbackup;
+    cout << "MergeSort:" << endl;
+    o = v1.size() / 2;
+    if(v1.size() % 2 == 1) o++;
+    test(mergesort, n, v1, 0, o);
+    v1 = vbackup;
+    cout << "QuickSort:" << endl;
+    test(quickSort, n, v1, 0, v1.size() - 1);
+    v1 = vbackup;
+    cout << "QuickSortMed:" << endl;
+    test(quickSortMed, n, v1, 0, v1.size() - 1);
+    v1 = vbackup;
+    cout << "QuickSortNonStable:" << endl;
+    ticksStart = clock();
+    sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v1.begin(), v1.end());
+    ticksStart = clock();
+    sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    shuffle(v1.begin(), v1.end() - v1.size() + v1.size() / 10, g);
+    ticksStart = clock();
+    sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
+    v1 = vbackup;
+    cout << "QuickSortStable:" << endl;
+    ticksStart = clock();
+    stable_sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    stable_sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v1.begin(), v1.end());
+    ticksStart = clock();
+    stable_sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    shuffle(v1.begin(), v1.end() - v1.size() + v1.size() / 10, g);
+    ticksStart = clock();
+    stable_sort(v1.begin(), v1.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
+    n = 1e7;
+    cout << "Size: " << n << endl;
+    for(int i = 0;i < v2.size();i++) v2[i] = rand();
+    vbackup = v2;
+    cout << "PyramidSort:" << endl;
+    test(pyramidSort, n, v2);
+    v2 = vbackup;
+    cout << "MergeSort:" << endl;
+    o = v2.size() / 2;
+    if(v2.size() % 2 == 1) o++;
+    test(mergesort, n, v2, 0, o);
+    v2 = vbackup;
+    cout << "QuickSort:" << endl;
+    test(quickSort, n, v2, 0, v2.size() - 1);
+    v2 = vbackup;
+    cout << "QuickSortMed:" << endl;
+    test(quickSortMed, n, v2, 0, v2.size() - 1);
+    v2 = vbackup;
+    cout << "QuickSortNonStable:" << endl;
+    ticksStart = clock();
+    sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v2.begin(), v2.end());
+    ticksStart = clock();
+    sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    shuffle(v2.begin(), v2.end() - v2.size() + v2.size() / 10, g);
+    ticksStart = clock();
+    sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
+    v2 = vbackup;
+    cout << "QuickSortStable:" << endl;
+    ticksStart = clock();
+    stable_sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "non_sorted: " << endl;
+    cout << timeUsed << endl;
+    ticksStart = clock();
+    stable_sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted: " << endl;
+    cout << timeUsed << endl;
+    reverse(v2.begin(), v2.end());
+    ticksStart = clock();
+    stable_sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "sorted, reversed: " << endl;
+    cout << timeUsed << endl;
+    shuffle(v2.begin(), v2.end() - v2.size() + v2.size() / 10, g);
+    ticksStart = clock();
+    stable_sort(v2.begin(), v2.end());
+    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
+    cout << "90% sorted" << endl;
+    cout << timeUsed << endl;
     return 0;
 }
